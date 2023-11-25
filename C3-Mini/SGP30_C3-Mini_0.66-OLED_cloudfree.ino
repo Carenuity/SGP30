4db1117a00911587fa34762361d09b3818aadbfb
TIMOTHYMWALA <mwalatimo@gmail.com>
2023-11-25 20:52:43 +0300

    Update SGP30_C3-Mini_0.66-OLED_cloudfree.ino

--- C3-Mini/SGP30_C3-Mini_0.66-OLED_cloudfree.ino
+++ C3-Mini/SGP30_C3-Mini_0.66-OLED_cloudfree.ino
@@ -3,16 +3,15 @@
 #include <Adafruit_SSD1306.h>
 #include <Adafruit_SGP30.h>
 
-#define OLED_RESET 7 // GPIO0 corresponds to pin 7 for the C3-mini 
+#define OLED_RESET 0 // GPIO0
 Adafruit_SSD1306 display(OLED_RESET);
 
 Adafruit_SGP30 sgp30; 
 
 void setup()
 {
-Wire.begin(8,10);
+Wire.begin();
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
-  
 
   if (sgp30.begin() == false)
   {
@@ -27,30 +26,29 @@ Wire.begin(8,10);
 void loop()
 {
 
-  delay(1000);
-
   display.clearDisplay();
   display.setTextSize(1);
-  display.setCursor(40, 10);
   display.setTextColor(WHITE);
+  display.setCursor(33, 8);
+  display.println("Air.Quality");
 
   if (sgp30.IAQmeasure())
   {
-
+    display.setCursor(35, 16);
     display.println("CO2: ");
     display.setTextSize(1);
-    display.setCursor(75, 10);
+    display.setCursor(60, 16);
     display.println(sgp30.eCO2);
 
+    display.setCursor(35, 25);
     display.setTextSize(1);
-     display.setCursor(40, 23);
     display.println("TVOC: ");
-
+    display.setCursor(65, 25);
     display.setTextSize(1);
-     display.setCursor(75, 23);
     display.println(sgp30.TVOC);
 
     display.display();
+     delay(4000);
   }
   else
   {
